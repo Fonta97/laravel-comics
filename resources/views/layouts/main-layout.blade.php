590a0293a1315d6af100a345dcc503575020330c
Fonta97 <fontana.alessandro97@gmail.com>
2024-02-14 11:23:06 +0100

    gg wp mai visto qualcosa di così speedrunnato come lezione

--- resources/views/layouts/main-layout.blade.php
+++ resources/views/layouts/main-layout.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,11 +10,14 @@
 
     @yield('head')
 </head>
+
 <body>
     <div class="text-center">
         @include('components.header')
+        @include('components.main')
         @yield('content')
         @include('components.footer')
     </div>
 </body>
+
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,25 @@
 @extends('layouts.main-layout')
 @section('head')
-    <title>Home</title>
+<title>Home</title>
 @endsection
 @section('content')
-    <h1>Hello, World!</h1>
+
+<main>
+    <div class="container">
+        @foreach ($comics as $comic)
+        <a href="#">
+            <div class="thumb">
+                <div class="img-thumb-container">
+                    <img src="{{$comic['thumb']}}">
+                </div>
+                <div class="thumb-title">
+                    {{$comic['title']}}
+                </div>
+            </div>
+        </a>
+        @endforeach
+        <a href="#" class="button">LOAD MORE</a>
+    </div>
+</main>
+
 @endsection
